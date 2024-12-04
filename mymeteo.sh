3e983d91055c20827d5f25022c8e309706ee991a
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-12-05 00:37:31 +0100

    download data from api

--- mymeteo.sh
+++ mymeteo.sh
@@ -14,12 +14,48 @@ for opt; do
     esac
 done
 
-nominatim_res="https://nominatim.openstreetmap.org/search.php?q=$city&format=jsonv2"
-curl -sk ${nominatim_res} -o temp_json.json
+getLocation(){
+  nominatim_res="https://nominatim.openstreetmap.org/search.php?q=$1&format=jsonv2";
+  curl -sk ${nominatim_res} -o temp.json;
 
-lat=$( ./jq.exe '.[0].lat' temp_json.json)
-lon=$( ./jq.exe '.[0].lon' temp_json.json)
+  lat=$( ./jq.exe '.[0].lat' temp.json);
+  lon=$( ./jq.exe '.[0].lon' temp.json);
 
-echo ${lat} ${lon}
+  rm temp.json
 
-rm temp_json.json
+  echo "$lat $lon"
+}
+
+downloadStations(){
+  stations="https://danepubliczne.imgw.pl/api/data/synop/"
+  curl -sk ${stations} -o temp_stations.json
+  size=$(./jq.exe 'length' temp_stations.json)
+
+  echo "{" > temp_locations.json
+
+  for ((i=0; i<size; i++))
+  do
+    check_city=$(./jq.exe ".[$i].stacja" temp_stations.json | tr -d '"')
+    check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
+
+    location=$(getLocation "$check_city")
+    lat=$(echo "$location" | cut -d ' ' -f 1)
+    lon=$(echo "$location" | cut -d ' ' -f 2)
+
+    station_data="\"$check_city\": {\"lat\": $lat, \"lon\": $lon},"
+
+    echo $station_data >> temp_locations.json;
+  done
+
+  echo "\"size\": $size, "\"lastupdate\"":\"$(date "+%Y-%m-%d")\" }" >> temp_locations.json
+}
+
+
+if [[ -e "temp_locations.json" ]]; then
+  check_date=$(./jq.exe ".lastupdate" temp_locations.json | tr -d '"')
+  if [[ "$check_date" != "$(date "+%Y-%m-%d")" ]]; then
+    downloadStations
+  fi
+else
+  downloadStations
+fi

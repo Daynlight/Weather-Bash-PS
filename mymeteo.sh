b4495d4f7a9377de5b75bcd9f48d6f47a2c91756
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-12-05 03:31:04 +0100

    rework

--- mymeteo.sh
+++ mymeteo.sh
@@ -1,9 +1,62 @@
 #!/bin/bash
 
-getLocation(){
-  size=$(./jq.exe 'length' temp_locations.json)
+checkIfNeedToDownloadData(){
+    if [[ -e "temp_locations.json" ]]; then
+    size=$(./jq.exe 'length' temp_locations.json)
+    check_date=$(./jq.exe '.[0].lastupdate' temp_locations.json | tr -d '"')
+    if [[ "$check_date" != "$(date "+%Y-%m-%d")" ]]; then
+      downloadData
+    fi
+  else
+    downloadData
+  fi
+}
+
+downloadData(){
+  stations="https://danepubliczne.imgw.pl/api/data/synop/"
+  curl -sk ${stations} -o temp_stations.json
+  size=$(./jq.exe 'length' temp_stations.json)
+
+  echo "[{\"lastupdate\":\"$(date "+%Y-%m-%d")\"}]" > temp_locations.json
+
+  for ((i=0; i<size; i++))
+  do
+    check_city=$(./jq.exe ".[$i].stacja" temp_stations.json | tr -d '"')
+    check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
+    checkEntries "$check_city" "0" > /dev/null 2>&1
+  done
+}
+
+checkEntries(){
   find=0
   if (( $2 == 1 )); then
+    res=$(searchForEntry "$1")
+    if [[ -n $res ]]; then
+      find=1
+      echo $res
+    fi
+  fi
+  if (( find == 0 || $2 == 0 )); then
+    addEntry "$1"
+  fi
+}
+
+addEntry(){
+  nominatim_res="https://nominatim.openstreetmap.org/search.php?q=$1&format=jsonv2";
+  curl -sk ${nominatim_res} -o temp.json;
+
+  lat=$( ./jq.exe '.[0].lat' temp.json);
+  lon=$( ./jq.exe '.[0].lon' temp.json);
+  new_location="{\"name\":\"$1\", \"lat\":$lat, \"lon\":$lon}"
+  ./jq.exe ". += [$new_location]" temp_locations.json > temp_locations_new.json && mv temp_locations_new.json temp_locations.json
+  
+  rm temp.json
+
+  echo "$lat $lon"
+}
+
+searchForEntry(){
+  size=$(./jq.exe 'length' temp_locations.json)
   for ((i=0; i<size; i++))
   do
     name=$(./jq.exe ".[$i].name" temp_locations.json | tr -d '"')
@@ -15,53 +68,9 @@ getLocation(){
       break
     fi
   done
-  fi
-  if (( find == 0 || $2 == 0 )); then
-    nominatim_res="https://nominatim.openstreetmap.org/search.php?q=$1&format=jsonv2";
-    curl -sk ${nominatim_res} -o temp.json;
-
-    lat=$( ./jq.exe '.[0].lat' temp.json);
-    lon=$( ./jq.exe '.[0].lon' temp.json);
-
-    new_location="{\"name\":\"$1\", \"lat\":$lat, \"lon\":$lon}"
-    ./jq.exe ". += [$new_location]" temp_locations.json > temp_locations_new.json && mv temp_locations_new.json temp_locations.json
-    
-    rm temp.json
-
-    echo "$lat $lon"
-  fi
 }
 
-downloadStations(){
-  stations="https://danepubliczne.imgw.pl/api/data/synop/"
-  curl -sk ${stations} -o temp_stations.json
-  size=$(./jq.exe 'length' temp_stations.json)
-
-  echo "[{\"lastupdate\":\"$(date "+%Y-%m-%d")\"}]" > temp_locations.json
-
-  for ((i=0; i<size; i++))
-  do
-    check_city=$(./jq.exe ".[$i].stacja" temp_stations.json | tr -d '"')
-    check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
-    res=$(getLocation "$check_city" "0")
-  done
-}
-
-printTemp(){
-  if [[ -e "temp_locations.json" ]]; then
-    size=$(./jq.exe 'length' temp_locations.json)
-    check_date=$(./jq.exe '.[0].lastupdate' temp_locations.json | tr -d '"')
-    if [[ "$check_date" != "$(date "+%Y-%m-%d")" ]]; then
-      downloadStations
-    fi
-  else
-    downloadStations
-  fi
-  check_city=$1
-  check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
-  city_location=$(getLocation "$check_city" "1")
-  city_lat=$(echo "$city_location" | cut -d ' ' -f 1 | xargs | awk '{print $1}')
-  city_lon=$(echo "$city_location" | cut -d ' ' -f 2 | xargs | awk '{print $1}')
+findNearest(){
   size=$(./jq.exe 'length' temp_stations.json)
   min=1
   for ((i=2; i<size+1; i++))
@@ -70,14 +79,18 @@ printTemp(){
     min_lon=$(./jq.exe ".[$min].lon" temp_locations.json | tr -d '"')
     lat=$(./jq.exe ".[$i].lat" temp_locations.json | tr -d '"')
     lon=$(./jq.exe ".[$i].lon" temp_locations.json | tr -d '"')
-    distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$city_lat $city_lon $lat $lon")
-    min_distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$city_lat $city_lon $min_lat $min_lon")
+    distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$1 $2 $lat $lon")
+    min_distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$1 $2 $min_lat $min_lon")
     if (( $(awk '{print ($1 < $2)}' <<< "$distance $min_distance") )); then
       min=$i
     fi
   done
 
-  min=$min-1
+  echo $min
+}
+
+printUI(){
+  min=$1-1
   id_stacji=$(./jq.exe ".[$min].id_stacji" temp_stations.json | tr -d '"')
   stacja=$(./jq.exe ".[$min].stacja" temp_stations.json | tr -d '"')
   temperatura=$(./jq.exe ".[$min].temperatura" temp_stations.json | tr -d '"')
@@ -97,6 +110,22 @@ printTemp(){
   echo "cisnienie: $cisnienie hPa"
 }
 
+main(){
+  checkIfNeedToDownloadData
+  
+  check_city=$1
+  check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
+  city_location=$(checkEntries "$check_city" "1")
+  city_lat=$(echo "$city_location" | cut -d ' ' -f 1 | xargs | awk '{print $1}')
+  city_lon=$(echo "$city_location" | cut -d ' ' -f 2 | xargs | awk '{print $1}')
+
+  printUI $(findNearest "$city_lat" "$city_lon")
+
+}
+
+
+
+
 help(){
   echo "-c, --city <Your City> - Check weather for city"
   exit 0
@@ -105,9 +134,8 @@ help(){
 for opt; do
     case "$opt" in
       -c | --city)
-        printTemp "$2" ;;
+        main "$2" ;;
       -h | --help)
       help ;;
     esac
 done
-

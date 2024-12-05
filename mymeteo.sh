862c336e5ca4588acb7bae669441a85b981c0ed6
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-12-05 04:34:02 +0100

    smal optimisation

--- mymeteo.sh
+++ mymeteo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 checkIfNeedToDownloadData(){
-    if [[ -e "temp_locations.json" ]]; then
-    size=$(./jq.exe 'length' temp_locations.json)
-    check_date=$(./jq.exe '.[0].lastupdate' temp_locations.json | tr -d '"')
-    if [[ "$check_date" != "$(date "+%Y-%m-%d")" ]]; then
+    if [[ -e "~/.cache/mymeteo/temp_locations.json" ]]; then
+    size=$(./jq.exe 'length' ~/.cache/mymeteo/temp_locations.json)
+    check_date=$(./jq.exe '.[0].lastupdate' ~/.cache/mymeteo/temp_locations.json | tr -d '"')
+    if [[ "$check_date" != "$(date "+%Y-%m-%d %H")" ]]; then
       downloadData
     fi
   else
@@ -14,14 +14,16 @@ checkIfNeedToDownloadData(){
 
 downloadData(){
   stations="https://danepubliczne.imgw.pl/api/data/synop/"
-  curl -sk ${stations} -o temp_stations.json
-  size=$(./jq.exe 'length' temp_stations.json)
+  curl -sk ${stations} -o ~/.cache/mymeteo/temp_locations_stations.json
+  size=$(./jq.exe 'length' ~/.cache/mymeteo/temp_locations_stations.json)
 
-  echo "[{\"lastupdate\":\"$(date "+%Y-%m-%d")\"}]" > temp_locations.json
-
+  if [[ ! -f ~/.cache/mymeteo/temp_locations.json ]]; then
+    echo "[{\"lastupdate\":\"$(date "+%Y-%m-%d %H")\"}]" > ~//.cache/mymeteo/temp_locations.json
+  fi
+ 
   for ((i=0; i<size; i++))
   do
-    check_city=$(./jq.exe ".[$i].stacja" temp_stations.json | tr -d '"')
+    check_city=$(./jq.exe ".[$i].stacja" ~//.cache/mymeteo/temp_locations_stations.json | tr -d '"')
     check_city=$(echo "$check_city" | sed 'y/ąćęłńóśźżĄĆĘŁŃÓŚŹŻ /acelnoszzACELNOSZZ_/')
     check_city=$(echo "$check_city" | tr '[:upper:]' '[:lower:]')
     checkEntries "$check_city" "0" > /dev/null 2>&1
@@ -30,41 +32,42 @@ downloadData(){
 
 checkEntries(){
   find=0
-  if (( $2 == 1 )); then
-    res=$(searchForEntry "$1")
-    if [[ -n $res ]]; then
-      find=1
+  res=$(searchForEntry "$1")
+  if [[ -n $res ]]; then
+    find=1
+    if (( $2 == 1 )); then
       echo $res
     fi
   fi
-  if (( find == 0 || $2 == 0 )); then
+  if (( find == 0 )); then
     addEntry "$1"
   fi
 }
 
 addEntry(){
-  nominatim_res="https://nominatim.openstreetmap.org/search.php?q=$1&format=jsonv2";
-  curl -sk ${nominatim_res} -o temp.json;
+  sleep 1s
+  nominatim_res="https://nominatim.openstreetmap.org/search.php?city=$1&country=Poland&format=jsonv2";
+  curl -sk ${nominatim_res} -o ~/.cache/mymeteo/temp.json;
 
-  lat=$( ./jq.exe '.[0].lat' temp.json);
-  lon=$( ./jq.exe '.[0].lon' temp.json);
+  lat=$( ./jq.exe '.[0].lat' ~/.cache/mymeteo/temp.json);
+  lon=$( ./jq.exe '.[0].lon' ~/.cache/mymeteo/temp.json);
   new_location="{\"name\":\"$1\", \"lat\":$lat, \"lon\":$lon}"
-  ./jq.exe ". += [$new_location]" temp_locations.json > temp_locations_new.json && mv temp_locations_new.json temp_locations.json
+  ./jq.exe ". += [$new_location]" ~/.cache/mymeteo/temp_locations.json > ~/.cache/mymeteo/temp_locations_new.json && mv ~/.cache/mymeteo/temp_locations_new.json ~/.cache/mymeteo/temp_locations.json
   
-  rm temp.json
+  rm ~/.cache/mymeteo/temp.json
 
   echo "$lat $lon"
 }
 
 searchForEntry(){
-  size=$(./jq.exe 'length' temp_locations.json)
+  size=$(./jq.exe 'length' ~/.cache/mymeteo/temp_locations.json)
   for ((i=0; i<size; i++))
   do
-    name=$(./jq.exe ".[$i].name" temp_locations.json | tr -d '"')
+    name=$(./jq.exe ".[$i].name" ~/.cache/mymeteo/temp_locations.json | tr -d '"')
     if [[ "$name" == "$1" ]]; then
       find=1
-      lat=$( ./jq.exe ".[$i].lat" temp_locations.json);
-      lon=$( ./jq.exe ".[$i].lon" temp_locations.json);
+      lat=$( ./jq.exe ".[$i].lat" ~/.cache/mymeteo/temp_locations.json);
+      lon=$( ./jq.exe ".[$i].lon" ~/.cache/mymeteo/temp_locations.json);
       echo "$lat $lon"
       break
     fi
@@ -72,14 +75,14 @@ searchForEntry(){
 }
 
 findNearest(){
-  size=$(./jq.exe 'length' temp_stations.json)
+  size=$(./jq.exe 'length' ~/.cache/mymeteo/temp_locations_stations.json)
   min=1
   for ((i=2; i<size+1; i++))
   do
-    min_lat=$(./jq.exe ".[$min].lat" temp_locations.json | tr -d '"')
-    min_lon=$(./jq.exe ".[$min].lon" temp_locations.json | tr -d '"')
-    lat=$(./jq.exe ".[$i].lat" temp_locations.json | tr -d '"')
-    lon=$(./jq.exe ".[$i].lon" temp_locations.json | tr -d '"')
+    min_lat=$(./jq.exe ".[$min].lat" ~/.cache/mymeteo/temp_locations.json | tr -d '"')
+    min_lon=$(./jq.exe ".[$min].lon" ~/.cache/mymeteo/temp_locations.json | tr -d '"')
+    lat=$(./jq.exe ".[$i].lat" ~/.cache/mymeteo/temp_locations.json | tr -d '"')
+    lon=$(./jq.exe ".[$i].lon" ~/.cache/mymeteo/temp_locations.json | tr -d '"')
     distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$1 $2 $lat $lon")
     min_distance=$(awk '{print ($3-$1)*($3-$1) + ($4-$2)*($4-$2)}' <<< "$1 $2 $min_lat $min_lon")
     if (( $(awk '{print ($1 < $2)}' <<< "$distance $min_distance") )); then
@@ -92,14 +95,14 @@ findNearest(){
 
 printUI(){
   min=$1-1
-  id_stacji=$(./jq.exe ".[$min].id_stacji" temp_stations.json | tr -d '"')
-  stacja=$(./jq.exe ".[$min].stacja" temp_stations.json | tr -d '"')
-  temperatura=$(./jq.exe ".[$min].temperatura" temp_stations.json | tr -d '"')
-  predkosc_wiatru=$(./jq.exe ".[$min].predkosc_wiatru" temp_stations.json | tr -d '"')
-  kierunek_wiatru=$(./jq.exe ".[$min].kierunek_wiatru" temp_stations.json | tr -d '"')
-  wilgotnosc_wzgledna=$(./jq.exe ".[$min].wilgotnosc_wzgledna" temp_stations.json | tr -d '"')\
-  suma_opadu=$(./jq.exe ".[$min].suma_opadu" temp_stations.json | tr -d '"')
-  cisnienie=$(./jq.exe ".[$min].cisnienie" temp_stations.json | tr -d '"')
+  id_stacji=$(./jq.exe ".[$min].id_stacji" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  stacja=$(./jq.exe ".[$min].stacja" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  temperatura=$(./jq.exe ".[$min].temperatura" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  predkosc_wiatru=$(./jq.exe ".[$min].predkosc_wiatru" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  kierunek_wiatru=$(./jq.exe ".[$min].kierunek_wiatru" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  wilgotnosc_wzgledna=$(./jq.exe ".[$min].wilgotnosc_wzgledna" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')\
+  suma_opadu=$(./jq.exe ".[$min].suma_opadu" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
+  cisnienie=$(./jq.exe ".[$min].cisnienie" ~/.cache/mymeteo/temp_locations_stations.json | tr -d '"')
 
   echo "$stacja [$id_stacji] / $(date +"%Y-%m-%d %H:%M")"
   echo ""
@@ -112,6 +115,7 @@ printUI(){
 }
 
 main(){
+  mkdir -p ~/.cache/mymeteo/
   checkIfNeedToDownloadData
   
   check_city=$1
@@ -128,6 +132,7 @@ main(){
 
 help(){
   echo "-c, --city <Your City> - Check weather for city"
+  echo "Author: Daniel Stodulski"
   exit 0
 }
 
